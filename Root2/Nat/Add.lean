5810690f2205c8f702a30b810bb2ed2f8f1f8a15
RustyYato <rustyyato@gmail.com>
2023-05-01 18:02:05 -0700

    remove uses of simp in Add

--- Root2/Nat/Add.lean
+++ Root2/Nat/Add.lean
@@ -1,78 +1,59 @@
-import Root2.Nat
+import Root2.Nat.Cmp
 
-@[simp]
 def nat.add (a b : nat) := match a with
   | nat.zero => b
   | nat.inc n => nat.inc (add n b)
 
-@[simp]
 theorem nat.add_zero (a: nat) : add zero a = a := by
-  unfold add
-  simp
+  trivial
 
-@[simp]
 theorem nat.add_zero_r (a: nat) : add a zero = a := by
   unfold add
   match a with
-    | nat.zero => simp
+    | nat.zero => trivial
     | nat.inc a₀ => simp; apply nat.add_zero_r
 
-@[simp]
 theorem nat.add_ab_eq_zero (a b: nat) : add a b = zero -> a = nat.zero /\ b = nat.zero := by
   unfold add
   match a with
-    | nat.zero => simp; intro b_eq_zero; simp; assumption
-    | nat.inc a₀ => simp
+    | nat.zero => trivial
+    | nat.inc a₀ => intro; trivial
 
-@[simp]
 theorem nat.add_inc (a b: nat) : add a (inc b) = inc (add a b) := by
   unfold add
   match a with
     | nat.zero => simp
     | nat.inc a₀ => simp; apply nat.add_inc
 
-@[simp]
 theorem nat.add_inc_r (a b: nat) : add (inc a) b = inc (add a b) := by
-  unfold add
   match a with
-    | nat.zero => simp
-    | nat.inc a₀ => simp
+    | nat.zero => trivial
+    | nat.inc a₀ => trivial
 
 theorem nat.add_comm (a b: nat) : add a b = add b a := by
   match a with
   | nat.zero => match b with
-    | nat.zero => simp
-    | nat.inc b₀ => 
-      unfold add
-      simp
+    | nat.zero => trivial
+    | nat.inc b₀ => rw [nat.add_zero, nat.add_zero_r]
   | nat.inc a₀ => match b with
-    | nat.zero => simp
+    | nat.zero => rw [nat.add_zero, nat.add_zero_r]
     | nat.inc b₀ =>
-      simp
-      apply nat.add_comm
+      repeat rw [nat.add_inc, nat.add_inc_r]
+      rw [nat.add_comm a₀ b₀]
 
-@[simp]
-theorem nat.add_irr (a b c: nat) : add a b = add a c <-> b = c := by
-  apply Iff.intro
-  . match a with
-      | nat.zero => simp; exact id
-      | nat.inc a₀ =>simp; intro _ ; apply (nat.add_irr a₀ b c).mp; assumption
-  . match a with
-      | nat.zero => simp; exact id
-      | nat.inc a₀ =>simp; intro _ ; apply (nat.add_irr a₀ b c).mpr; assumption
+theorem nat.add_irr (a b c: nat) : (add a b = add a c) = (b = c) := by
+  cases a
+  rw [nat.add_zero, nat.add_zero]
+  rw [nat.add_inc_r, nat.add_inc_r]
+  rw [nat.eq_inc_irr]
+  apply nat.add_irr
 
 theorem nat.add_assoc (a b c: nat) : add a (add b c) = add (add a b) c := by
   match a with
-  | nat.zero => match b with
-    | nat.zero => simp
-    | nat.inc b₀ => 
-      unfold add
-      simp
-  | nat.inc a₀ => match b with
-    | nat.zero => simp
-    | nat.inc b₀ =>
-      simp
-      apply nat.add_assoc
+  | nat.zero => repeat rw [nat.add_zero]
+  | nat.inc a₀ => 
+      repeat rw [nat.add_inc_r]
+      rw [nat.add_assoc a₀]
 
 theorem nat.add_perm0 (a b c: nat) : add a (add b c) = add (add a b) c := by
   apply nat.add_assoc
@@ -97,8 +78,7 @@ theorem nat.add_perm5 (a b c: nat) : add a (add b c) = add (add c b) a := by
   rw [nat.add_comm b c, nat.add_comm]
 
 theorem nat.add_perm6 (a b c: nat) : add a (add b c) = add a (add c b) := by
-  simp
-  apply nat.add_comm
+  rw [nat.add_comm b]
 
 theorem nat.add_perm7 (a b c: nat) : add a (add b c) = add b (add a c) := by
   rw [nat.add_assoc, nat.add_comm a b, ←nat.add_assoc]
