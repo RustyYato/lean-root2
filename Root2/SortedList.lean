2dd17e499b552dc03753c26fe7c5ea935a98874f
RustyYato <rustyyato@gmail.com>
2023-05-19 16:57:39 -0700

    prove concat sorted comm and any and all

--- Root2/SortedList.lean
+++ Root2/SortedList.lean
@@ -107,21 +107,21 @@ def List.concat_sorted.induction [Compare α]
   {P: List α -> List α -> Prop}
   (empty_left: ∀ bs, P [] bs)
   (empty_right: ∀ a as, P (a::as) [])
-  (induct_lt: ∀a b as bs, P (a::as) bs -> P (a::as) (b::bs))
-  (induct_eq: ∀a b as bs, P as bs -> P (a::as) (b::bs))
-  (induct_gt: ∀a b as bs, P as (b::bs) -> P (a::as) (b::bs)):
+  (induct_lt: ∀a b as bs, a < b -> P (a::as) bs -> P (a::as) (b::bs))
+  (induct_eq: ∀a b as bs, a = b -> P as bs -> P (a::as) (b::bs))
+  (induct_gt: ∀a b as bs, a > b -> P as (b::bs) -> P (a::as) (b::bs)):
   ∀ as bs, P as bs := fun as bs =>
   match as with
 | [] => empty_left bs
 | a::as => match bs with
 | [] => empty_right a as
-| b::bs => match Compare.ord a b with
+| b::bs => match h:Compare.ord a b with
 | .Eq =>
-  induct_eq a b as bs (List.concat_sorted.induction empty_left empty_right induct_lt induct_eq induct_gt as bs)
+  induct_eq a b as bs (Compare.ord_implies_eq h) (List.concat_sorted.induction empty_left empty_right induct_lt induct_eq induct_gt as bs)
 | .Less =>
-  induct_lt a b as bs (List.concat_sorted.induction empty_left empty_right induct_lt induct_eq induct_gt (a::as) bs)
+  induct_lt a b as bs h (List.concat_sorted.induction empty_left empty_right induct_lt induct_eq induct_gt (a::as) bs)
 | .Greater =>
-  induct_gt a b as bs (List.concat_sorted.induction empty_left empty_right induct_lt induct_eq induct_gt as (b::bs))
+  induct_gt a b as bs (Compare.flip h) (List.concat_sorted.induction empty_left empty_right induct_lt induct_eq induct_gt as (b::bs))
 termination_by List.concat_sorted.induction a b => a.length + b.length
 decreasing_by {
   simp_wf
@@ -351,12 +351,242 @@ section concat_sorted
       try { admit }
     }
   
-  theorem list_sorted_nonempty_left [Compare α] {{ a b: α }} (as bs : List α)
+  theorem nonempty_left [Compare α] {{ a b: α }} (as bs : List α)
       (aas: (a::as).sorted) (bbs : (b::bs).sorted) (b_le_a: b <= a):
       List.sorted (a::List.concat_sorted as (b::bs)) := (list_sorted_nonempty  as bs aas bbs).left b_le_a
 
-  theorem list_sorted_nonempty_right [Compare α] {{ a b: α }} (as bs : List α)
+  theorem nonempty_right [Compare α] {{ a b: α }} (as bs : List α)
       (aas: (a::as).sorted) (bbs : (b::bs).sorted)
       (a_le_b: a <= b) : List.sorted (b::List.concat_sorted (a::as) bs) := (list_sorted_nonempty  as bs aas bbs).right a_le_b
 
 end concat_sorted
+
+theorem concat_sorted.comm 
+  [Compare α]
+  {{alist blist: List α}}
+  (a_sorted: alist.sorted) (b_sorted: blist.sorted)
+  : alist.concat_sorted blist = blist.concat_sorted alist := by
+  unfold List.concat_sorted
+  match alist, blist with
+  | [], x => simp; cases x <;> rfl
+  | x, [] => simp; cases x <;> rfl
+  | a::as, b::bs => 
+    simp
+    cases h:Compare.ord a b <;> simp
+    {
+      rw [Compare.flip h]
+      simp
+      apply concat_sorted.comm
+      assumption
+      apply pop; assumption
+    }
+    {
+      rw [Compare.flip h]; simp
+      have : a = b := Compare.ord_implies_eq h
+      apply And.intro
+      assumption
+      apply And.intro
+      exact this.symm
+      apply concat_sorted.comm <;> (apply pop; assumption)
+    }
+    {
+      rw [Compare.flip h]; simp
+      apply concat_sorted.comm
+      apply pop; assumption
+      assumption
+    }
+termination_by concat_sorted.comm => alist.length + blist.length
+decreasing_by {
+  simp_wf
+  
+  try {
+    rw [Nat.add_succ]
+    rw [Nat.add_comm (Nat.succ _), Nat.add_succ]
+    apply Nat.succ_lt_succ
+    apply Nat.lt_succ_self
+  }
+  try {
+    rw [Nat.add_succ]
+    rw [Nat.add_comm (Nat.succ _), Nat.add_succ]
+    apply Nat.succ_lt_succ
+    rw [Nat.add_comm (Nat.succ _), Nat.add_succ]
+    apply Nat.lt_succ_self
+  }
+  try {
+    apply Nat.add_lt_add <;> (apply Nat.lt_succ_self)
+  }
+}
+
+#print axioms concat_sorted.comm
+
+theorem concat_sorted.keeps_sorted
+  [inst: Compare α]
+  (alist blist: List α)
+  (a_sorted: alist.sorted) (b_sorted: blist.sorted)
+  : (alist.concat_sorted blist).sorted := 
+by
+  match h₁:alist, h₂:blist with
+  | [], _ => simp; assumption
+  | _, [] => unfold List.concat_sorted; split; assumption; exact a_sorted
+  | [a], [b] =>
+    unfold List.concat_sorted
+    split
+    assumption
+    next a' ns list_a_eq => {
+      match ns with 
+      | _ :: _ => simp at list_a_eq
+      | [] => 
+      simp at list_a_eq
+      rw [←list_a_eq,empty_left, empty_left, empty_right]
+      split
+      simp
+      apply Or.inr
+      apply Compare.ord_symm
+      assumption
+      simp
+      apply Or.inl
+      assumption
+      simp
+      apply Or.inl
+      apply Compare.of_flip
+      assumption
+    }
+  | a :: as, b :: bs =>
+    simp
+    cases h:Compare.ord a b <;> simp
+    {
+      apply nonempty_right
+      assumption
+      assumption
+      exact Or.inl h
+    }
+    {
+      apply And.intro
+      {
+        apply Or.inr
+        apply Compare.ord_symm
+        assumption
+      }
+      match as with
+      | [] => apply list_sorted_snd_fst_empty; assumption
+      | a'::as' =>
+        apply nonempty_right
+        exact a_sorted.right
+        assumption
+        have a_eq_b := Compare.ord_implies_eq h
+        rw [←a_eq_b]
+        exact a_sorted.left
+    }
+    {
+      apply nonempty_left
+      assumption
+      assumption
+      apply Or.inl
+      apply Compare.of_flip
+      assumption
+    }
+
+#print axioms concat_sorted.keeps_sorted
+
+theorem concat_sorted_all [Compare α] {{P : α -> Prop}} :
+  ∀{{alist blist: List α}},
+  (alist.allP P) -> (blist.allP P) ->
+  (alist.concat_sorted blist).allP P := by
+  apply List.concat_sorted.induction
+  {
+    intro bs _ all_bs
+    rw [empty_right]
+    exact all_bs
+  }
+  {
+    intro a as all_as _
+    rw [empty_left]
+    exact all_as
+  }
+  {
+    intro a b as bs a_lt_b prev all_as all_bs
+    unfold List.concat_sorted
+    simp only
+    rw [a_lt_b]
+    simp
+    apply And.intro
+    exact all_bs.left
+    exact prev all_as all_bs.right
+  }
+  {
+    intro a b as bs a_eq_b prev all_as all_bs
+    rw [a_eq_b]
+    simp
+    rw [Compare.ord_id]
+    simp
+    apply And.intro
+    exact all_bs.left
+    apply And.intro
+    exact all_bs.left
+    exact prev all_as.right all_bs.right
+  }
+  {
+    intro a b as bs a_gt_b prev all_as all_bs
+    unfold List.concat_sorted
+    simp
+    rw [Compare.flip a_gt_b]
+    simp
+    apply And.intro
+    exact all_as.left
+    exact prev all_as.right all_bs
+  }
+
+#print axioms concat_sorted_all 
+
+theorem concat_sorted_any [Compare α] {{P : α -> Prop}} :
+  ∀{{alist blist: List α}},
+  (alist.anyP P) ∨ (blist.anyP P) ->
+  (alist.concat_sorted blist).anyP P := by
+  apply List.concat_sorted.induction
+  {
+    intro bs any_as_or_bs
+    rw [empty_right]
+    simp at any_as_or_bs
+    exact any_as_or_bs
+  }
+  {
+    intro a as any_as_or_bs
+    rw [empty_left]
+    simp at any_as_or_bs
+    exact any_as_or_bs
+  }
+  {
+    intro a b as bs a_lt_b prev any_as_or_bs
+    unfold List.concat_sorted
+    simp only
+    rw [a_lt_b]
+    simp
+    exact match any_as_or_bs with 
+    | .inl any_as => Or.inr (prev (Or.inl any_as))
+    | .inr (.inl pb) => Or.inl pb
+    | .inr (.inr any_bs) => Or.inr (prev (Or.inr any_bs))
+  }
+  {
+    intro a b as bs a_eq_b prev any_as_or_bs
+    simp
+    rw [Compare.ord_from_eq a_eq_b]
+    simp
+    exact match any_as_or_bs with 
+    | .inl (.inl pa) => Or.inl pa
+    | .inl (.inr any_as) => Or.inr (Or.inr (prev (Or.inl any_as)))
+    | .inr (.inl pb) => Or.inr (Or.inl pb)
+    | .inr (.inr any_bs) => Or.inr (Or.inr (prev (Or.inr any_bs)))
+  }
+  {
+    intro a b as bs a_gt_b prev any_as_or_bs
+    unfold List.concat_sorted
+    simp
+    rw [Compare.flip a_gt_b]
+    simp
+    exact match any_as_or_bs with 
+    | .inl (.inl pa) => Or.inl pa
+    | .inl (.inr any_as) => Or.inr (prev (Or.inl any_as))
+    | .inr any_bs => Or.inr (prev (Or.inr any_bs))
+  }
+
+#print axioms concat_sorted_any
