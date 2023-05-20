069b397396f736e5b56d1e3eb5c20f6af7383dde
RustyYato <rustyyato@gmail.com>
2023-05-20 14:12:22 -0700

    move gcd prime factor

--- Root2.lean
+++ Root2.lean
@@ -2,6 +2,7 @@ import Root2.Divisible.DivRem
 import Root2.Prime.PrimeFactor
 import Root2.Nat.Reduction
 import Root2.Nat.Gcd
+import Root2.Nat.Gcd.PrimeFactor
 
 theorem root2_is_irrational : ∀ a b: nat, (nat.mul a a) = (nat.mul nat.zero.inc.inc (nat.mul b b)) -> False := by
   intro a b a_sq_eq_2_b_sq
